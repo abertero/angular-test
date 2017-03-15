06049d84f5172bbb1afd47fb84b3a42e85b29820
Aldo Bertero <abertero@antica.com>
2017-03-15 14:40:07 -0300

    Moduled detail for hero

--- angular-tour-heroes/src/app/app.component.ts
+++ angular-tour-heroes/src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-  </div>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
   <h2>My Heroes</h2>
   <ul class="heroes">
@@ -77,11 +71,6 @@ export class AppComponent  {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
